104188d6fa10e199c96e668aa71d3fd0bb6c5c09
patridifusa <p.rufino.a@gmail.com>
2022-11-19 19:56:43 +0000

    Define scripts files and config Typescript compiler

---
+++ dist/scripts/index.js
@@ -0,0 +1,7 @@
+import { character } from './personaje.js';
+import { characters } from './personajes.js';
+const renderContent = () => {
+    character();
+    characters();
+};
+renderContent();
---
+++ dist/scripts/personajes.js
@@ -0,0 +1,3 @@
+export const characters = () => {
+    console.log('Hola, soy characters function');
+};
